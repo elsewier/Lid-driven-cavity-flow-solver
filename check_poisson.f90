be92616d93c216c3eadb125fae69e756dd490584
elsewier <yesildag18@itu.edu.tr>
2025-09-11 21:01:37 +0300

    check_poisson is added

--- check_poisson.f90
+++ check_poisson.f90
@@ -9,7 +9,6 @@ program check_poisson
     integer :: iblock, k, i_basis, j_basis
     integer :: N, info, global_point_offset, row_index, col_idx
     real(dp), parameter :: pi = acos(-1.0d0)
-    real(dp) :: x, y
 
     ! linear system arrays
     real(dp), allocatable :: A_psi(:,:), b_rhs(:), d_psi_numerical(:)
@@ -19,10 +18,15 @@ program check_poisson
     real(dp), allocatable :: M_psi(:,:), psi_exact_vec(:), d_psi_exact(:)
 
     ! local variables
-    real(dp)  :: N_val, M_val, dM_dy, d2N_dx2, d2M_dy2
+    real(dp)  :: x, y, N_val, M_val, dM_dy, d2N_dx2, d2M_dy2
 
-    ! Error calculation
+    ! Pointwise error calculation variables
+    real(dp) :: psi_numerical_at_point, psi_exact_at_point, error_at_point
+
+    ! L2 Error calculation
     real(dp) :: l2_err_num, l2_err_den, l2_relative_error
+
+    ! ... (The top part of the program remains exactly the same) ...
 
     write(*,*) "================================================"
     write(*,*) "   Poisson solver verification (MMS)  "
@@ -40,76 +44,69 @@ program check_poisson
     allocate(A_psi(N, N), b_rhs(N), d_psi_numerical(N), ipiv(N))
     A_psi = 0.0d0; b_rhs = 0.0d0;
 
-    write(*,*) "2. Assembling MMS Poisson system..."
+    ! 2. ASSEMBLE MMS POISSON SYSTEM
+    write(*,*) "2. Assembling MMS Poisson System..."
     global_point_offset = 0
     do iblock = 1, NUM_BLOCKS
       do k = 1, blocks(iblock)%N_x * blocks(iblock)%N_y
         row_index = global_point_offset + k
         x = blocks(iblock)%colloc_pts(k, 1)
         y = blocks(iblock)%colloc_pts(k, 2)
-
         A_psi(row_index, :) = 0.0d0
-
         select case (blocks(iblock)%boundary_types(k))
-
         case (BTYPE_INTERIOR)
-          do j_basis = 1, blocks(iblock)%N_y; do i_basis = 1, blocks(iblock)%N_x
-            col_idx = get_global_psi_index(blocks, iblock, i_basis, j_basis)
-            N_val   = bspline_basis_physical(i_basis, blocks(iblock)%P_x, blocks(iblock)%knots_x, x, blocks(iblock)%xmin, blocks(iblock)%xmax)
-            M_val   = bspline_basis_physical(j_basis, blocks(iblock)%P_y, blocks(iblock)%knots_y, y, blocks(iblock)%ymin, blocks(iblock)%ymax)
-            d2N_dx2 = bspline_deriv2_physical(i_basis, blocks(iblock)%P_x, blocks(iblock)%knots_x, x, blocks(iblock)%xmin, blocks(iblock)%xmax)
-            d2M_dy2 = bspline_deriv2_physical(j_basis, blocks(iblock)%P_y, blocks(iblock)%knots_y, y, blocks(iblock)%ymin, blocks(iblock)%ymax)
-            A_psi(row_index, col_idx) = d2N_dx2 * M_val + N_val * d2M_dy2
+          do j_basis=1,blocks(iblock)%N_y; do i_basis=1,blocks(iblock)%N_x
+            col_idx = get_global_psi_index(blocks,iblock,i_basis,j_basis)
+            N_val=bspline_basis_physical(i_basis,blocks(iblock)%P_x,blocks(iblock)%knots_x,x,blocks(iblock)%xmin,blocks(iblock)%xmax)
+            M_val=bspline_basis_physical(j_basis,blocks(iblock)%P_y,blocks(iblock)%knots_y,y,blocks(iblock)%ymin,blocks(iblock)%ymax)
+            d2N_dx2=bspline_deriv2_physical(i_basis,blocks(iblock)%P_x,blocks(iblock)%knots_x,x,blocks(iblock)%xmin,blocks(iblock)%xmax)
+            d2M_dy2=bspline_deriv2_physical(j_basis,blocks(iblock)%P_y,blocks(iblock)%knots_y,y,blocks(iblock)%ymin,blocks(iblock)%ymax)
+            A_psi(row_index,col_idx) = d2N_dx2*M_val + N_val*d2M_dy2
           enddo; enddo
-          b_rhs(row_index) = f_exact(x, y)
-
+          b_rhs(row_index) = f_exact(x,y)
         case (BTYPE_WALL)
-          do j_basis = 1, blocks(iblock)%N_y; do i_basis = 1, blocks(iblock)%N_x
-            col_idx = get_global_psi_index(blocks, iblock, i_basis, j_basis)
-            N_val   = bspline_basis_physical(i_basis, blocks(iblock)%P_x, blocks(iblock)%knots_x, x, blocks(iblock)%xmin, blocks(iblock)%xmax)
-            M_val   = bspline_basis_physical(j_basis, blocks(iblock)%P_y, blocks(iblock)%knots_y, y, blocks(iblock)%ymin, blocks(iblock)%ymax)
+          do j_basis=1,blocks(iblock)%N_y; do i_basis=1,blocks(iblock)%N_x
+            col_idx = get_global_psi_index(blocks,iblock,i_basis,j_basis)
+            N_val=bspline_basis_physical(i_basis,blocks(iblock)%P_x,blocks(iblock)%knots_x,x,blocks(iblock)%xmin,blocks(iblock)%xmax)
+            M_val=bspline_basis_physical(j_basis,blocks(iblock)%P_y,blocks(iblock)%knots_y,y,blocks(iblock)%ymin,blocks(iblock)%ymax)
             A_psi(row_index, col_idx) = N_val * M_val
           enddo; enddo
           b_rhs(row_index) = psi_exact(x, y)
-
         case (BTYPE_MOVING_LID)
-          do j_basis = 1, blocks(iblock)%N_y; do i_basis = 1, blocks(iblock)%N_x
-            col_idx = get_global_psi_index(blocks, iblock, i_basis, j_basis)
-            N_val   = bspline_basis_physical(i_basis, blocks(iblock)%P_x, blocks(iblock)%knots_x, x, blocks(iblock)%xmin, blocks(iblock)%xmax)
-            dM_dy   = bspline_deriv1_physical(j_basis, blocks(iblock)%P_y, blocks(iblock)%knots_y, y, blocks(iblock)%ymin, blocks(iblock)%ymax)
+          do j_basis=1,blocks(iblock)%N_y; do i_basis=1,blocks(iblock)%N_x
+            col_idx = get_global_psi_index(blocks,iblock,i_basis,j_basis)
+            N_val=bspline_basis_physical(i_basis,blocks(iblock)%P_x,blocks(iblock)%knots_x,x,blocks(iblock)%xmin,blocks(iblock)%xmax)
+            dM_dy=bspline_deriv1_physical(j_basis,blocks(iblock)%P_y,blocks(iblock)%knots_y,y,blocks(iblock)%ymin,blocks(iblock)%ymax)
             A_psi(row_index, col_idx) = N_val * dM_dy
           enddo; enddo
-          b_rhs(row_index) = dpsi_dy_exact(x, y)
-
+          b_rhs(row_index) = dpsi_dy_exact(x,y)
         case (BTYPE_INTERFACE)
           b_rhs(row_index) = 0.0d0
           if (iblock == 1) then
-            ! C0 continuity: psi_b1 - psi_b2 = 0
-            do j_basis = 1, blocks(1)%N_y; do i_basis = 1, blocks(1)%N_x
-              col_idx = get_global_psi_index(blocks, 1, i_basis, j_basis)
-              N_val   = bspline_basis_physical(i_basis, blocks(1)%P_x, blocks(1)%knots_x, x, blocks(1)%xmin, blocks(1)%xmax)
-              M_val   = bspline_basis_physical(j_basis, blocks(1)%P_y, blocks(1)%knots_y, y, blocks(1)%ymin, blocks(1)%ymax)
-              A_psi(row_index, col_idx) = A_psi(row_index, col_idx) + (N_val * M_val)
+            do j_basis=1,blocks(1)%N_y; do i_basis=1,blocks(1)%N_x
+              col_idx=get_global_psi_index(blocks,1,i_basis,j_basis)
+              N_val=bspline_basis_physical(i_basis,blocks(1)%P_x,blocks(1)%knots_x,x,blocks(1)%xmin,blocks(1)%xmax)
+              M_val=bspline_basis_physical(j_basis,blocks(1)%P_y,blocks(1)%knots_y,y,blocks(1)%ymin,blocks(1)%ymax)
+              A_psi(row_index,col_idx)=N_val*M_val
             enddo; enddo
-            do j_basis = 1, blocks(2)%N_y; do i_basis = 1, blocks(2)%N_x
-              col_idx = get_global_psi_index(blocks, 2, i_basis, j_basis)
-              N_val   = bspline_basis_physical(i_basis, blocks(2)%P_x, blocks(2)%knots_x, x, blocks(2)%xmin, blocks(2)%xmax)
-              M_val   = bspline_basis_physical(j_basis, blocks(2)%P_y, blocks(2)%knots_y, y, blocks(2)%ymin, blocks(2)%ymax)
-              A_psi(row_index, col_idx) = A_psi(row_index, col_idx) - (N_val * M_val)
+            do j_basis=1,blocks(2)%N_y; do i_basis=1,blocks(2)%N_x
+              col_idx=get_global_psi_index(blocks,2,i_basis,j_basis)
+              N_val=bspline_basis_physical(i_basis,blocks(2)%P_x,blocks(2)%knots_x,x,blocks(2)%xmin,blocks(2)%xmax)
+              M_val=bspline_basis_physical(j_basis,blocks(2)%P_y,blocks(2)%knots_y,y,blocks(2)%ymin,blocks(2)%ymax)
+              A_psi(row_index,col_idx)= - (N_val*M_val)
             enddo; enddo
           else if (iblock == 2) then
-            ! C1 continuity: d(psi_b1)/dy - d(psi_b2)/dy = 0
-            do j_basis = 1, blocks(1)%N_y; do i_basis = 1, blocks(1)%N_x
-              col_idx = get_global_psi_index(blocks, 1, i_basis, j_basis)
-              N_val   = bspline_basis_physical(i_basis, blocks(1)%P_x, blocks(1)%knots_x, x, blocks(1)%xmin, blocks(1)%xmax)
-              dM_dy   = bspline_deriv1_physical(j_basis, blocks(1)%P_y, blocks(1)%knots_y, y, blocks(1)%ymin, blocks(1)%ymax)
-              A_psi(row_index, col_idx) = A_psi(row_index, col_idx) + (N_val * dM_dy)
+            do j_basis=1,blocks(1)%N_y; do i_basis=1,blocks(1)%N_x
+              col_idx=get_global_psi_index(blocks,1,i_basis,j_basis)
+              N_val=bspline_basis_physical(i_basis,blocks(1)%P_x,blocks(1)%knots_x,x,blocks(1)%xmin,blocks(1)%xmax)
+              dM_dy=bspline_deriv1_physical(j_basis,blocks(1)%P_y,blocks(1)%knots_y,y,blocks(1)%ymin,blocks(1)%ymax)
+              A_psi(row_index,col_idx)=N_val*dM_dy
             enddo; enddo
-            do j_basis = 1, blocks(2)%N_y; do i_basis = 1, blocks(2)%N_x
-              col_idx = get_global_psi_index(blocks, 2, i_basis, j_basis)
-              N_val   = bspline_basis_physical(i_basis, blocks(2)%P_x, blocks(2)%knots_x, x, blocks(2)%xmin, blocks(2)%xmax)
-              dM_dy   = bspline_deriv1_physical(j_basis, blocks(2)%P_y, blocks(2)%knots_y, y, blocks(2)%ymin, blocks(2)%ymax)
-              A_psi(row_index, col_idx) = A_psi(row_index, col_idx) - (N_val * dM_dy)
+            do j_basis=1,blocks(2)%N_y; do i_basis=1,blocks(2)%N_x
+              col_idx=get_global_psi_index(blocks,2,i_basis,j_basis)
+              N_val=bspline_basis_physical(i_basis,blocks(2)%P_x,blocks(2)%knots_x,x,blocks(2)%xmin,blocks(2)%xmax)
+              dM_dy=bspline_deriv1_physical(j_basis,blocks(2)%P_y,blocks(2)%knots_y,y,blocks(2)%ymin,blocks(2)%ymax)
+              A_psi(row_index,col_idx)= - (N_val*dM_dy)
             enddo; enddo
           endif
         end select
@@ -117,6 +114,7 @@ program check_poisson
       global_point_offset = global_point_offset + (blocks(iblock)%N_x * blocks(iblock)%N_y)
     enddo
 
+    ! 3. SOLVE THE SYSTEM FOR NUMERICAL COEFFICIENTS
     write(*,*) "3. Solving for numerical B-spline coefficients..."
     d_psi_numerical = b_rhs
     call dgesv(N, 1, A_psi, N, ipiv, d_psi_numerical, N, info)
@@ -125,15 +123,14 @@ program check_poisson
         stop
     endif
 
+    ! 4. FIND THE EXACT COEFFICIENTS FOR L2 NORM CALCULATION
     write(*,*) "4. Finding exact B-spline coefficients..."
     allocate(M_psi(N, N), psi_exact_vec(N), d_psi_exact(N))
     M_psi = 0.0d0
-
     global_point_offset = 0
     do iblock = 1, NUM_BLOCKS; do k = 1, blocks(iblock)%N_x * blocks(iblock)%N_y
         row_index = global_point_offset + k
-        x = blocks(iblock)%colloc_pts(k, 1)
-        y = blocks(iblock)%colloc_pts(k, 2)
+        x = blocks(iblock)%colloc_pts(k, 1); y = blocks(iblock)%colloc_pts(k, 2)
         do j_basis = 1, blocks(iblock)%N_y; do i_basis = 1, blocks(iblock)%N_x
             col_idx = get_global_psi_index(blocks, iblock, i_basis, j_basis)
             N_val   = bspline_basis_physical(i_basis, blocks(iblock)%P_x, blocks(iblock)%knots_x, x, blocks(iblock)%xmin, blocks(iblock)%xmax)
@@ -141,22 +138,57 @@ program check_poisson
             M_psi(row_index, col_idx) = N_val * M_val
         enddo; enddo
     enddo; global_point_offset = global_point_offset + (blocks(iblock)%N_x * blocks(iblock)%N_y); enddo
-
     global_point_offset = 0
     do iblock = 1, NUM_BLOCKS; do k = 1, blocks(iblock)%N_x * blocks(iblock)%N_y
         row_index = global_point_offset + k
-        x = blocks(iblock)%colloc_pts(k, 1)
-        y = blocks(iblock)%colloc_pts(k, 2)
+        x = blocks(iblock)%colloc_pts(k, 1); y = blocks(iblock)%colloc_pts(k, 2)
         psi_exact_vec(row_index) = psi_exact(x, y)
     enddo; global_point_offset = global_point_offset + (blocks(iblock)%N_x * blocks(iblock)%N_y); enddo
-
     d_psi_exact = psi_exact_vec
     call dgesv(N, 1, M_psi, N, ipiv, d_psi_exact, N, info)
-    if (info /= 0) then
-        write(*,*) "DGESV failed to find exact coeffs. INFO=", info
-        stop
-    endif
+    if (info /= 0) then; write(*,*) "DGESV failed to find exact coeffs. INFO=", info; stop; endif
 
+    ! =============================================================================
+    ! 5. NEW: CALCULATE AND WRITE POINTWISE ERROR TO FILE
+    ! =============================================================================
+    write(*,*) "5. Calculating pointwise error and writing to file..."
+    OPEN(unit=30, file='poisson_error.csv', status='replace')
+    WRITE(30, '(A)') '"x","y","psi_numerical","psi_exact","error"'
+
+    global_point_offset = 0
+    do iblock = 1, NUM_BLOCKS
+      do k = 1, blocks(iblock)%N_x * blocks(iblock)%N_y
+        row_index = global_point_offset + k
+        x = blocks(iblock)%colloc_pts(k, 1)
+        y = blocks(iblock)%colloc_pts(k, 2)
+
+        ! Reconstruct the numerical solution at this point
+        psi_numerical_at_point = 0.0_dp
+        do j_basis = 1, blocks(iblock)%N_y
+          do i_basis = 1, blocks(iblock)%N_x
+            col_idx = get_global_psi_index(blocks, iblock, i_basis, j_basis)
+            N_val = bspline_basis_physical(i_basis, blocks(iblock)%P_x, blocks(iblock)%knots_x, x, blocks(iblock)%xmin, blocks(iblock)%xmax)
+            M_val = bspline_basis_physical(j_basis, blocks(iblock)%P_y, blocks(iblock)%knots_y, y, blocks(iblock)%ymin, blocks(iblock)%ymax)
+            psi_numerical_at_point = psi_numerical_at_point + d_psi_numerical(col_idx) * N_val * M_val
+          enddo
+        enddo
+
+        ! Get the exact solution at this point
+        psi_exact_at_point = psi_exact(x, y)
+
+        ! Calculate the error
+        error_at_point = psi_numerical_at_point - psi_exact_at_point
+
+        ! Write to file
+        WRITE(30, '(F8.4,A,F8.4,A,E15.7,A,E15.7,A,E15.7)') &
+            x, ',', y, ',', psi_numerical_at_point, ',', psi_exact_at_point, ',', error_at_point
+      enddo
+      global_point_offset = global_point_offset + (blocks(iblock)%N_x * blocks(iblock)%N_y)
+    enddo
+    CLOSE(30)
+    write(*,*) "   ... Done. Data saved to poisson_error.csv"
+
+    ! 6. CALCULATE AND PRINT OVERALL L2 ERROR
     l2_err_num = sum((d_psi_numerical - d_psi_exact)**2)
     l2_err_den = sum(d_psi_exact**2)
     if (l2_err_den == 0.0_dp) then
@@ -165,9 +197,10 @@ program check_poisson
         l2_relative_error = sqrt(l2_err_num / l2_err_den)
     endif
 
-    write(*,*) "5. Results:"
+    write(*,*) "6. Results:"
     write(*, '(A, E12.5)') " L2 relative error in coefficients:", l2_relative_error
 
+    ! 7. CLEAN UP
     deallocate(blocks, A_psi, b_rhs, d_psi_numerical, ipiv)
     deallocate(M_psi, psi_exact_vec, d_psi_exact)
 
