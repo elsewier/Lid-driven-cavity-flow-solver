c6512dfdac813f376bceda7b9e7b1346f1688a51
elsewier <yesildag18@itu.edu.tr>
2025-09-11 18:01:42 +0300

    derivative test

--- grid.f90
+++ grid.f90
@@ -88,7 +88,9 @@ end subroutine initialize_block
         ! generate knot vectors
         if (block%id == 2) then
             break_ratio = CORNER_X / DOMAIN_XMAX
-            call generate_stretched_knots(block%P_x, block%N_x, block%knots_x, break_point_ratio=break_ratio) ! piecewise
+            ! Create knots with C^0 continuity (multiplicity = p) at the break point
+            call generate_stretched_knots(block%P_x, block%N_x, block%knots_x, &
+                break_point_ratio=break_ratio, break_knot_multiplicity=block%P_x)
         else
             call generate_stretched_knots(block%P_x, block%N_x, block%knots_x)
         endif
@@ -131,12 +133,19 @@ end subroutine initialize_block
 end subroutine generate_grid
 
 
-SUBROUTINE generate_stretched_knots(p, num_basis, knots_out, break_point_ratio)
+! =============================================================================
+! MODIFIED SUBROUTINE: generate_stretched_knots
+! 
+! Added optional argument `break_knot_multiplicity` to introduce repeated
+! internal knots at the `break_point_ratio`.
+! =============================================================================
+SUBROUTINE generate_stretched_knots(p, num_basis, knots_out, break_point_ratio, break_knot_multiplicity)
         integer, intent(in)     :: p, num_basis
         real(dp), intent(out)   :: knots_out(:)
-        real(dp), intent(IN), optional :: break_point_ratio
+        real(dp), intent(in), optional :: break_point_ratio
+        integer, intent(in), optional  :: break_knot_multiplicity ! <-- NEW ARGUMENT
 
-        integer  :: i, m, break_index, n1, n2
+        integer  :: i, m, break_index, n1, n2, k_mult, n2_new, i_start_part2
         real(dp) :: s, stretched_s
         real(dp), PARAMETER :: stretch_factor = 2.5d0
 
@@ -145,22 +154,59 @@ SUBROUTINE generate_stretched_knots(p, num_basis, knots_out, break_point_ratio)
         knots_out(num_basis + 1 : m) = 1.0_dp
 
         if (present(break_point_ratio)) then
-            ! Two-part stretching for knots
+            ! --- Multi-part stretching with optional multiplicity ---
+
+            ! Set multiplicity. Default is 1 (a simple knot)
+            k_mult = 1
+            if (present(break_knot_multiplicity)) then
+                k_mult = break_knot_multiplicity
+            endif
+
+            ! Original logic to find the index for the break
             break_index = NINT(REAL(num_basis - p, dp) * break_point_ratio) + p + 1
             n1 = break_index - (p + 1)
-            n2 = num_basis - break_index + 1
+            n2 = num_basis - break_index + 1 ! Original denominator calculation
+
+            ! --- Part 1: Stretch from 0.0 to break_point_ratio ---
             do i = p + 2, break_index
                 s = REAL(i - (p + 1), dp) / REAL(n1, dp)
                 stretched_s = 0.5d0 * (tanh(stretch_factor * (2.0d0 * s - 1.0d0)) + 1.0d0)
                 knots_out(i) = 0.0_dp + stretched_s * break_point_ratio
             enddo
-            do i = break_index + 1, num_basis
-                s = REAL(i - break_index, dp) / REAL(n2, dp)
-                stretched_s = 0.5d0 * (tanh(stretch_factor * (2.0d0 * s - 1.0d0)) + 1.0d0)
-                knots_out(i) = break_point_ratio + stretched_s * (1.0_dp - break_point_ratio)
+            ! At this point, knots_out(break_index) = break_point_ratio
+
+            ! --- Part 2: Add multiplicity ---
+            ! Add (k_mult - 1) more knots at the same location
+            ! This loop runs (k_mult - 1) times. If k_mult=1, it doesn't run.
+            do i = break_index + 1, break_index + k_mult - 1
+                knots_out(i) = break_point_ratio
             enddo
+
+            ! --- Part 3: Stretch from break_point_ratio to 1.0 ---
+            ! This loop must now start *after* the repeated knots
+            i_start_part2 = break_index + k_mult
+            
+            ! Adjust original denominator 'n2' for knots "used" by multiplicity
+            n2_new = n2 - (k_mult - 1) 
+            
+            if (n2_new > 0) then
+                ! There are still knot slots left to fill with a stretch
+                do i = i_start_part2, num_basis
+                    ! Offset numerator to start from 1 (relative to the new start)
+                    s = REAL(i - (i_start_part2 - 1), dp) / REAL(n2_new, dp)
+                    stretched_s = 0.5d0 * (tanh(stretch_factor * (2.0d0 * s - 1.0d0)) + 1.0d0)
+                    knots_out(i) = break_point_ratio + stretched_s * (1.0_dp - break_point_ratio)
+                enddo
+            else
+                ! k_mult was so large it used all remaining knot slots.
+                ! Set all remaining internal knots to the break_point_ratio.
+                do i = i_start_part2, num_basis
+                    knots_out(i) = break_point_ratio
+                enddo
+            endif
+
         else
-            ! Simple one-part stretching for knots
+            ! --- Simple one-part stretching (no change) ---
             do i = p + 2, num_basis
                 s = REAL(i - p - 1, dp) / REAL(num_basis - p, dp)
                 knots_out(i) = 0.5d0 * (tanh(stretch_factor * (2.0d0 * s - 1.0d0)) + 1.0d0)
